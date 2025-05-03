5459174aa11648898ab73d2d9e39d1a900522d44
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2025-05-03 16:10:59 -0600

    Domain core added, movies and user repositories

---
+++ apirest_ratings/domain/repositories/postgres_movierepository.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	domain "raitings.com/api/entities"
+)
+
+type PostgresMovieRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresMovieRepository(db *sql.DB) *PostgresMovieRepository {
+	return &PostgresMovieRepository{
+		db: db,
+	}
+}
+
+func (r *PostgresMovieRepository) Save(ctx context.Context, movie *domain.Movie) error {
+	query := "INSERT INTO movies (movieid, title, genres) VALUES ($1, $2, $3)"
+	_, err := r.db.ExecContext(ctx, query, movie.Movieid, movie.Title, movie.Genres)
+
+	if err != nil {
+		return fmt.Errorf("adapter: failed to save user %s to postgres: %w", movie.Movieid, err)
+	}
+	fmt.Printf("Adapter: User %s saved to PostgreSQL.\n", movie.Movieid)
+	return nil
+}
+
+func (r *PostgresMovieRepository) Close() error {
+	return r.db.Close()
+}
